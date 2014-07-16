04e4371f123f423687c87b36dede35583ab637fa
orangemug <info@orangemug.co.uk>
2014-07-16 09:28:32 +0100

    Fixed test values.

--- test/index.js
+++ test/index.js
@@ -12,14 +12,14 @@ test('stylesheet 1', function (t) {
 
 	el.style.width = "50px";
 	fixie.fixScale();
-	t.equal(el.querySelector(".c1").getBoundingClientRect().width, 5);
-	t.equal(el.querySelector(".c2").getBoundingClientRect().width, 10);
+	t.equal(el.querySelector(".c1").getBoundingClientRect().width, 10);
+	t.equal(el.querySelector(".c2").getBoundingClientRect().width, 15);
 	t.equal(el.querySelector(".c3").getBoundingClientRect().width, 20);
 
 	el.style.width = "200px";
 	fixie.fixScale();
-	t.equal(el.querySelector(".c1").getBoundingClientRect().width, 20);
-	t.equal(el.querySelector(".c2").getBoundingClientRect().width, 40);
+	t.equal(el.querySelector(".c1").getBoundingClientRect().width, 40);
+	t.equal(el.querySelector(".c2").getBoundingClientRect().width, 60);
 	t.equal(el.querySelector(".c3").getBoundingClientRect().width, 80);
 
 	t.end();
